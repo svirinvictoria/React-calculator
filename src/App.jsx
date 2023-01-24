680374706a378c2f17a38cf3f3b1a9449b62fa9c
Vika <svirin.victoria@gmail.com>
2023-01-24 10:43:29 +0100

    the value of the pressed button is presented on the display

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import React, { } from "react";
+
 import "./App.scss";
 // import CalcButton from "./components/CalcButton/CalcButton";
 // import CalcScreen from "./components/CalcScreen/CalcScreen";
 import Calculator from "./components/Calculator/Calculator";
 
 function App() {
+  
+
+
   return (
     <div className="App">
       <header className="App-header">
